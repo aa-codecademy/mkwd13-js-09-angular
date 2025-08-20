65307978e81cee30bdde640878cbdd0ba9b0f046
borisovski-borche <borisovski.borce@gmail.com>
2025-08-20 20:39:37 +0200

    class-07-02

--- G1/class-07-directives-pipes/user-app/src/app/app.ts
+++ G1/class-07-directives-pipes/user-app/src/app/app.ts
@@ -1,15 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
 import { Header } from './core/components/header/header';
 import { usersMock } from './feature/users/users-mock';
 import { User } from './feature/users/models/user-model';
 import { UserCard } from './feature/users/components/user-card/user-card';
+import { FilterPipe } from './core/pipes/filter-pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, UserCard],
+  imports: [Header, UserCard, FilterPipe, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   users = signal<User[]>(usersMock);
+  searchValue = model('');
+
+  //KEYUP IMPLEMENTATION
+  // searchValue = signal('');
+  // onInputChange(event: Event) {
+  //   const target = event.target as HTMLInputElement;
+
+  //   console.log(target.value);
+  //   this.searchValue.set(target.value);
+  // }
 }
--- G1/class-07-directives-pipes/user-app/src/app/feature/users/components/user-card/user-card.ts
+++ G1/class-07-directives-pipes/user-app/src/app/feature/users/components/user-card/user-card.ts
@@ -3,10 +3,16 @@ import { User } from '../../models/user-model';
 import { HoverShadowDirective } from '../../../../core/directives/hover-shadow-directive';
 import { ToggleAddressDirective } from '../../../../core/directives/toggle-address-directive';
 import { CommonModule } from '@angular/common';
+import { ShortenPipe } from '../../../../core/pipes/shorten-pipe';
 
 @Component({
   selector: 'app-user-card',
-  imports: [HoverShadowDirective, ToggleAddressDirective, CommonModule],
+  imports: [
+    HoverShadowDirective,
+    ToggleAddressDirective,
+    CommonModule,
+    ShortenPipe,
+  ],
   templateUrl: './user-card.html',
   styleUrl: './user-card.scss',
 })
