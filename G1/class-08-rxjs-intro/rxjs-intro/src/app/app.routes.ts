f22c938bc9f6b051f8ebddaead2eb9d26aa4f7f0
borisovski-borche <75560716+borisovski-borche@users.noreply.github.com>
2025-08-22 18:41:15 +0200

    class-08-01

---
+++ G1/class-08-rxjs-intro/rxjs-intro/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { Observables } from './components/observables/observables';
+import { Subjects } from './components/subjects/subjects';
+import { Operators } from './components/operators/operators';
+import { Signals } from './components/signals/signals';
+
+export const routes: Routes = [
+  //   { path: '', redirectTo: 'observables', pathMatch: 'full' },
+  { path: 'observables', component: Observables },
+  { path: 'subjects', component: Subjects },
+  { path: 'operators', component: Operators },
+  { path: 'signals', component: Signals },
+];
