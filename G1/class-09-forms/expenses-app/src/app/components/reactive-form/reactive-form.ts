1b5d7ce557ef7142fb8d2601a9c54b7ab2e4e063
borisovski-borche <borisovski.borce@gmail.com>
2025-08-25 20:41:10 +0200

    class-09-02

---
+++ G1/class-09-forms/expenses-app/src/app/components/reactive-form/reactive-form.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-form.html',
+  styleUrl: './reactive-form.scss',
+})
+export class ReactiveForm {
+  expenseForm = this.generateExpenseForm();
+
+  generateExpenseForm() {
+    return new FormGroup({
+      title: new FormControl(''),
+      amount: new FormControl(''),
+      date: new FormControl('2024-08-09'),
+      priority: new FormControl('medium'),
+      comment: new FormControl(''),
+      type: new FormControl('cash'),
+    });
+  }
+
+  onFormSubmit() {
+    console.log(this.expenseForm.value);
+  }
+}
