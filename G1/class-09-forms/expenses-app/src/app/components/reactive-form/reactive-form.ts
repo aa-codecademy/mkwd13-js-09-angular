969b5b45479f0bc6e61395e9e01cd5a49f6166c2
borisovski-borche <borisovski.borce@gmail.com>
2025-09-01 18:32:42 +0200

    class-11-01

--- G1/class-09-forms/expenses-app/src/app/components/reactive-form/reactive-form.ts
+++ G1/class-09-forms/expenses-app/src/app/components/reactive-form/reactive-form.ts
@@ -1,27 +1,43 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, TitleCasePipe],
   templateUrl: './reactive-form.html',
   styleUrl: './reactive-form.scss',
 })
 export class ReactiveForm {
   expenseForm = this.generateExpenseForm();
 
+  paymentTypes: string[] = ['cash', 'credit'];
+
+  maxCommentLength = 90;
+
   generateExpenseForm() {
     return new FormGroup({
-      title: new FormControl(''),
-      amount: new FormControl(''),
-      date: new FormControl('2024-08-09'),
+      title: new FormControl('', Validators.required),
+      amount: new FormControl('', [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(500),
+      ]),
+      date: new FormControl('2025-09-01', Validators.required),
       priority: new FormControl('medium'),
-      comment: new FormControl(''),
+      comment: new FormControl('', Validators.maxLength(this.maxCommentLength)),
       type: new FormControl('cash'),
     });
   }
 
   onFormSubmit() {
+    console.log(this.expenseForm);
+
     console.log(this.expenseForm.value);
   }
 }
