47948c6e6359d21f563ce9fa5f6f3f6a6aff577d
borisovski-borche <75560716+borisovski-borche@users.noreply.github.com>
2025-09-05 18:33:44 +0200

    class-13-01

---
+++ G1/class-13-auth/reviews-app/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { Home } from './feature/home/home';
+import { Login } from './feature/auth/components/login/login';
+import { Register } from './feature/auth/components/register/register';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+];
