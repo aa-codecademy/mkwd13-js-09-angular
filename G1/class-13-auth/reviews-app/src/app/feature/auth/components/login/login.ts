afd3061588b449d026ad1995bfcc24848619ae37
borisovski-borche <75560716+borisovski-borche@users.noreply.github.com>
2025-09-05 19:45:14 +0200

    class-13-02

--- G1/class-13-auth/reviews-app/src/app/feature/auth/components/login/login.ts
+++ G1/class-13-auth/reviews-app/src/app/feature/auth/components/login/login.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Button } from '../../../../shared/components/button/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, Button],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
+  loginForm = this.generateForm();
 
+  generateForm() {
+    return new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  onFormSubmit() {
+    this.loginForm.markAllAsTouched();
+
+    if (this.loginForm.invalid) return;
+
+    console.log(this.loginForm.value);
+  }
 }
--- G1/class-13-auth/reviews-app/src/app/feature/auth/components/register/register.ts
+++ G1/class-13-auth/reviews-app/src/app/feature/auth/components/register/register.ts
@@ -1,11 +1,67 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Button } from '../../../../shared/components/button/button';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, Button],
   templateUrl: './register.html',
-  styleUrl: './register.scss'
+  styleUrl: './register.scss',
 })
 export class Register {
+  registerForm = this.generateForm();
 
+  generateForm() {
+    return new FormGroup(
+      {
+        firstName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ]),
+        lastName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ]),
+        username: new FormControl('', [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(30),
+        ]),
+        confirmPassword: new FormControl('', Validators.required),
+      },
+      this.passwordMatchValidator,
+    );
+  }
+
+  passwordMatchValidator(form: AbstractControl): null {
+    const passwordCtrl = form.get('password');
+    const confirmCtrl = form.get('confirmPassword');
+
+    console.log('PASSWORD VALUE: ', passwordCtrl.value);
+    console.log('CONFIRM VALUE: ', confirmCtrl.value);
+
+    return null;
+  }
+
+  onFormSubmit() {
+    this.registerForm.markAllAsTouched();
+
+    if (this.registerForm.invalid) return;
+
+    console.log(this.registerForm.value);
+  }
 }
